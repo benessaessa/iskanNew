1169855c288810ae445e6f89a99a6fa5d4b7fa36
mohamed <moh.essa2020@gmail.com>
2021-04-21 13:31:11 +0200

    new iskan

---
+++ js/file.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.formFilter').show();
+    $('.advanced').click(function(){
+        $('.formFilter').toggle();
+    });
+});
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+    },
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        440: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+    }
+});
